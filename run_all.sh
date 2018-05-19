fa45cde26ed18298f9d8a259ae8568606f323846
Tecnarca <mail.tecnarca@gmail.com>
2018-05-19 14:01:50 +0200

    Added simple bash script to run every program at once

---
+++ run_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# $1 == smaller matrix size, $2 == biggest matrix size, $3 == step of the matrix size
+
+rm -rf csv/ #remove old csv files
+mkdir csv/
+echo "Running monocore program"
+./bin/monocore $1 $2 $3
+echo "Running multicore program"
+./bin/multicore $1 $2 $3 4 #'4' is the number of threads, you might want change this
+echo "Running OpenMP program"
+./bin/openmp $1 $2 $3
+echo "Running CUDA program"
+./bin/cuda $1 $2 $3
+echo "Running CUBLAS program"
+./bin/cublas $1 $2 $3
+echo "Done. Now run 'python graph_plot.py' to see the data"
