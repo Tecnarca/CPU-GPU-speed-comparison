3e3f2281dcabdb3e4b2b968814ef8215e168756f
Tecnarca <mail.tecnarca@gmail.com>
2018-05-19 14:18:42 +0200

    Added compiled version of the source programs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CPU-GPU-speed-comparison
 A simple comparison between single thread program against multi-threading and CUDA
+
+If you are using the compiled version of the files, you can check what libraries you're missing by running the command
+ldd bin/*
+If you want to check a specific file (let's say, FILENAME), use
+ldd bin/FILENAME
--- compiler.sh
+++ compiler.sh
@@ -11,16 +11,16 @@
 # '-lopenblas' = linka cblas, serve per pre-processare le matrici prima di dare a cuSolver
 # nvcc è il compilatore CUDA
 
-rm *.o
-nvcc -c src/cuda.cu -o cuda.o -std=c++11 -w #crea il file oggetto per cuda
-nvcc -c src/cublas.cu -o cublas.o -std=c++11 -w #crea il file oggetto per cublas
-g++ -c src/monocore.cpp -o monocore.o -std=c++11 -w #crea il file oggetto per il monocore
-g++ -c src/multicore.cpp -o multicore.o -std=c++11 -w #crea il file oggetto per il multicore
-g++ -c src/utils.cpp -o utils.o -std=c++11 #crea il file oggetto per utils
-g++ -c src/openmp.cpp -o openmp.o -std=c++11 -w -fopenmp #fopenmp dice al compilatore di parallelizzare i "pragma parallel for"
-g++ monocore.o utils.o -o monocore -lm -std=c++11 #linka monocore e utils per creare l'exe
-g++ multicore.o utils.o -o multicore -lm -lpthread -std=c++11 #linka multicore e utils per creare l'exe
-g++ cuda.o utils.o -o cuda -lm -L/usr/local/cuda/lib64 -lcudart #linka multicore e utils per creare l'exe (probabilmente mancano delle opzioni)
-g++ cublas.o utils.o -o cublas -lm -L/usr/local/cuda/lib64 -lcusolver -lcudart -lcublas -lopenblas #linka multicore e utils per creare l'exe (probabilmente mancano delle opzioni)
-g++ openmp.o utils.o -o openmp -lm -std=c++11 -fopenmp -lpthread #fopenmp dice al compilatore di parallelizzare i "pragma parallel for"
-rm *.o
+mkdir -p objects/
+mkdir -p bin/
+nvcc -c src/cuda.cu -o objects/cuda.o -std=c++11 -w #crea il file oggetto per cuda
+nvcc -c src/cublas.cu -o objects/cublas.o -std=c++11 -w #crea il file oggetto per cublas
+g++ -c src/monocore.cpp -o objects/monocore.o -std=c++11 -w #crea il file oggetto per il monocore
+g++ -c src/multicore.cpp -o objects/multicore.o -std=c++11 -w #crea il file oggetto per il multicore
+g++ -c src/utils.cpp -o objects/utils.o -std=c++11 #crea il file oggetto per utils
+g++ -c src/openmp.cpp -o objects/openmp.o -std=c++11 -w -fopenmp #fopenmp dice al compilatore di parallelizzare i "pragma parallel for"
+g++ objects/monocore.o objects/utils.o -o bin/monocore -lm -std=c++11 #linka monocore e utils per creare l'exe
+g++ objects/multicore.o objects/utils.o -o bin/multicore -lm -lpthread -std=c++11 #linka multicore e utils per creare l'exe
+g++ objects/cuda.o objects/utils.o -o bin/cuda -lm -L/usr/local/cuda/lib64 -lcudart #linka multicore e utils per creare l'exe (probabilmente mancano delle opzioni)
+g++ objects/cublas.o objects/utils.o -o bin/cublas -lm -L/usr/local/cuda/lib64 -lcusolver -lcudart -lcublas -lopenblas #linka multicore e utils per creare l'exe (probabilmente mancano delle opzioni)
+g++ objects/openmp.o objects/utils.o -o objects/openmp -lm -std=c++11 -fopenmp -lpthread #fopenmp dice al compilatore di parallelizzare i "pragma parallel for"
