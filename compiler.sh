88f04c0f1c54bc146c8f6b2f15f104efca3132ae
davemesk <davide.meschini@hotmail.com>
2018-06-18 19:27:07 +0200

    creation of comments

--- compiler.sh
+++ compiler.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-# '-c' = compila e crea il file oggetto, senza linkare
-# '-o' = file di output
-# '-std=c++11' = usa lo standard c++ 2011
-# '-w' = sopprime gli warning di conversione da static string a char*
-# '-lm' = linka la libreria "math"
-# '-L/usr/local/cuda/lib64' = aggiunge /usr/local/cuda/lib64 ai percorsi da cui pescare le librerie
-# '-lcudart' = linka la libreria CUDA
-# '-lcublas' = linka la libreria cuBLAS
-# '-lcusolver' = linka il solver di cuBLAS
-# '-lopenblas' = linka cblas, serve per pre-processare le matrici prima di dare a cuSolver
-# nvcc è il compilatore CUDA
+# '-c' = compile and create the object file, without linking
+# '-o' = output file
+# '-std=c++11' = use the c++ 2011's standard
+# '-w' = change the conversion warning from static string to char*
+# '-lm' = link to the library "math"
+# '-L/usr/local/cuda/lib64' = add /usr/local/cuda/lib64 to the paths from where you take the libraries
+# '-lcudart' = link to the library CUDA
+# '-lcublas' = link to the library cuBLAS
+# '-lcusolver' = link to the cuBLAS's solver
+# '-lopenblas' = link to cblas, it's used to pre-process the matrices before giving them to cuSolver
+# nvcc it's the CUDA's compiler
 
 mkdir -p objects/
 mkdir -p bin/
-nvcc -c src/cuda.cu -o objects/cuda.o -std=c++11 -w #crea il file oggetto per cuda
-nvcc -c src/cublas.cu -o objects/cublas.o -std=c++11 -w #crea il file oggetto per cublas
-g++ -c src/monocore.cpp -o objects/monocore.o -std=c++11 -w #crea il file oggetto per il monocore
-g++ -c src/multicore.cpp -o objects/multicore.o -std=c++11 -w #crea il file oggetto per il multicore
-g++ -c src/utils.cpp -o objects/utils.o -std=c++11 #crea il file oggetto per utils
-g++ -c src/openmp.cpp -o objects/openmp.o -std=c++11 -w -fopenmp #fopenmp dice al compilatore di parallelizzare i "pragma parallel for"
-g++ objects/monocore.o objects/utils.o -o bin/monocore -lm -std=c++11 #linka monocore e utils per creare l'exe
-g++ objects/multicore.o objects/utils.o -o bin/multicore -lm -lpthread -std=c++11 #linka multicore e utils per creare l'exe
-g++ objects/cuda.o objects/utils.o -o bin/cuda -lm -L/usr/local/cuda/lib64 -lcudart #linka multicore e utils per creare l'exe (probabilmente mancano delle opzioni)
-g++ objects/cublas.o objects/utils.o -o bin/cublas -lm -L/usr/local/cuda/lib64 -lcusolver -lcudart -lcublas -lopenblas #linka multicore e utils per creare l'exe (probabilmente mancano delle opzioni)
-g++ objects/openmp.o objects/utils.o -o bin/openmp -lm -std=c++11 -fopenmp -lpthread #fopenmp dice al compilatore di parallelizzare i "pragma parallel for"
+nvcc -c src/cuda.cu -o objects/cuda.o -std=c++11 -w #create the object file for CUDA
+nvcc -c src/cublas.cu -o objects/cublas.o -std=c++11 -w #create the object file for cublas
+g++ -c src/monocore.cpp -o objects/monocore.o -std=c++11 -w #create the object file for the monocore
+g++ -c src/multicore.cpp -o objects/multicore.o -std=c++11 -w #create the object file for the multicore
+g++ -c src/utils.cpp -o objects/utils.o -std=c++11 #create the object file for utils
+g++ -c src/openmp.cpp -o objects/openmp.o -std=c++11 -w -fopenmp #fopenmp says to the compiler to parallelize the "pragma parallel for"
+g++ objects/monocore.o objects/utils.o -o bin/monocore -lm -std=c++11 #link monocore and utils to create the exe
+g++ objects/multicore.o objects/utils.o -o bin/multicore -lm -lpthread -std=c++11 #linka multicore e utils to create the exe
+g++ objects/cuda.o objects/utils.o -o bin/cuda -lm -L/usr/local/cuda/lib64 -lcudart #linka multicore e utils to create the exe (maybe some options are missing)
+g++ objects/cublas.o objects/utils.o -o bin/cublas -lm -L/usr/local/cuda/lib64 -lcusolver -lcudart -lcublas -lopenblas #linka multicore e utils to create the exe(maybe some options are missing)
+g++ objects/openmp.o objects/utils.o -o bin/openmp -lm -std=c++11 -fopenmp -lpthread #fopenmp says to che compiler to parallelize the "pragma parallel for"
